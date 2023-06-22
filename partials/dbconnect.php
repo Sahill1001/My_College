237556c8753f52831a726fdc31678df5685c46fb
Sahil Prasad <119433520+Sahill1001@users.noreply.github.com>
2023-06-22 18:03:20 +0530

    Update dbconnect.php

--- partials/dbconnect.php
+++ partials/dbconnect.php
@@ -26,20 +26,30 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-$exists = mysqli_query($conn, "select 1 from Students");
-$exists1 = mysqli_query($conn, "select 1 from contact");
+mysqli_query($conn, "SELECT * 
+FROM information_schema.tables
+WHERE table_schema = 'yourdb' 
+    AND table_name = 'testtable'
+LIMIT 1;");
 
-if (!$exists) {
-  $sql = "CREATE TABLE `college`.`Students` (`srn` INT(20) NOT NULL AUTO_INCREMENT , `fname` VARCHAR(50) NOT NULL , `email` VARCHAR(50) NOT NULL , `phone` VARCHAR(50) NOT NULL , `passwd` VARCHAR(255) NOT NULL , `gender` VARCHAR(10) NOT NULL , `sub1` VARCHAR(50) NOT NULL , `sub2` VARCHAR(50) NOT NULL , `sub3` VARCHAR(50) NOT NULL , `sub4` VARCHAR(50) NOT NULL , `sub5` VARCHAR(50) NOT NULL , `tdate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`srn`))";
-  if (!mysqli_query($conn, $sql)) {
-    echo "Error creating table: " . mysqli_error($conn);
-  }
+
+$sql = "CREATE TABLE IF NOT EXISTS `college`.`Students` (`srn` INT(20) NOT NULL AUTO_INCREMENT , `fname` VARCHAR(50) NOT NULL , `email` VARCHAR(50) NOT NULL , `phone` VARCHAR(50) NOT NULL , `passwd` VARCHAR(255) NOT NULL , `gender` VARCHAR(10) NOT NULL , `sub1` VARCHAR(50) NOT NULL , `sub2` VARCHAR(50) NOT NULL , `sub3` VARCHAR(50) NOT NULL , `sub4` VARCHAR(50) NOT NULL , `sub5` VARCHAR(50) NOT NULL , `tdate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`srn`))";
+if (!mysqli_query($conn, $sql)) {
+  echo "Error creating table: " . mysqli_error($conn);
 }
 
-if (!$exists1) {
-  $sql1 = "CREATE TABLE `college`.`contact` (`srn` INT(20) NOT NULL AUTO_INCREMENT , `fname` VARCHAR(50) NOT NULL , `email` VARCHAR(50) NOT NULL , `sub` VARCHAR(50) NOT NULL , `msg` VARCHAR(500) NOT NULL , `dtime` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`srn`))";
-  if (!mysqli_query($conn, $sql1)) {
-    echo "Error creating table: " . mysqli_error($conn);
-  }
+
+
+mysqli_query($conn, "SELECT * 
+FROM information_schema.tables
+WHERE table_schema = 'yourdb' 
+    AND table_name = 'testtable'
+LIMIT 1;");
+
+
+$sql1 = "CREATE TABLE IF NOT EXISTS `college`.`contact` (`srn` INT(20) NOT NULL AUTO_INCREMENT , `fname` VARCHAR(50) NOT NULL , `email` VARCHAR(50) NOT NULL , `sub` VARCHAR(50) NOT NULL , `msg` VARCHAR(500) NOT NULL , `dtime` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`srn`))";
+if (!mysqli_query($conn, $sql1)) {
+  echo "Error creating table: " . mysqli_error($conn);
 }
+
 ?>
